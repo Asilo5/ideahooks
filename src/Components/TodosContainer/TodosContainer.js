f3a172be4231affe8baf11fc7f8a05fbc5c1601e
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-26 15:57:06 -0700

    Remove console.logs

--- src/Components/TodosContainer/TodosContainer.js
+++ src/Components/TodosContainer/TodosContainer.js
@@ -4,7 +4,6 @@ import Todo from '../Todo/Todo';
 const TodosContainer = ({ideas, deleteIdea}) => {
 
     const theIdea = ideas.map((idea) => {
-        console.log('idea', idea.usersIdea)
        return <Todo key={idea.id} {...idea} deleteIdea={deleteIdea} />
     });
 

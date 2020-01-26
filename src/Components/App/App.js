c03c216c589e582ffb34be4a287b4a8eef52c2f7
Amanda Consuelo Sierra Lopez <asierralopez526@gmail.com>
2020-01-26 15:20:49 -0700

    Add files

---
+++ src/Components/App/App.js
@@ -0,0 +1,19 @@
+import React, {useState} from 'react';
+import './App.css';
+
+const App = () => {
+    
+   const [ todo, setTodo ] = useState([]);
+
+   
+
+  return (
+    <main>
+       <h1>Todo Hooks</h1>
+
+    </main>
+  )
+
+}
+
+export default App;
